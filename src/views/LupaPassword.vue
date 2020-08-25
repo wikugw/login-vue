c993fc2bcedfeb1305126a1543b03d571977c60d
wikugw <wikugw@gmail.com>
2020-08-26 06:38:03 +0700

    view reset password

--- src/views/LupaPassword.vue
+++ src/views/LupaPassword.vue
@@ -1,3 +1,40 @@
 <template>
-    <h2>Lupa password</h2>
+    <v-container>
+        <v-layout>
+            <v-flex sm12 md6 offset-md3>
+                <v-card>
+                    <v-col>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field
+                                label="Email"
+                                type="text"
+                                outline
+                            >
+                            </v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-btn color="primary">
+                                    Kirim Email
+                                </v-btn>
+                            </v-col>
+                            <v-col cols="12">
+                                <p>
+                                    Ingin Login Kembali?
+                                    <router-link to="/">
+                                        Login
+                                    </router-link>
+                                </p>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
+<style scoped>
+    .layout {
+        margin-top: 100px;
+    }
+</style>
--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,3 +1,15 @@
 <template>
-    <h2>Reset password</h2>
+  <v-container>
+    <v-layout wrap style="margin-top: 150px">
+      <v-flex md6 offset-md3>
+        <v-card>
+          <v-col>
+            <v-text-field label="Password Baru"></v-text-field>
+            <v-text-field label="Ulangi Password"></v-text-field>
+            <v-btn color="primary">Reset Password</v-btn>
+          </v-col>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
